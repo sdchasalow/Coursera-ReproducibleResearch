21498f642a2b7d7c60ac38b1feea1ad2faeb4273
Scott D. Chasalow <scott.chasalow@gmail.com>
2016-02-10 18:49:22 -0500

    New and improved hookup; still not working quite right...

--- hookup.R
+++ hookup.R
@@ -3,27 +3,58 @@ hookup <- function( who, p = n - 1 ) {
    if ( p >= n )
       stop( "p >= n, that is too weird.")
    whonum <- seq_len( n )
-   nummat <- array( NA, c(n, p) )
+   nmat <- array( NA, c(n, p) )
    counts <- numeric( n )
+   # Assign first partner to each person in who:
+   # See Examples under ?sample to understand why I use sample.int construct.
+   j <- 1
    for ( i in whonum ) {
-      picks <- sample( whonum[ whonum != i & counts < p], p )
-      print(picks)
-      counts[picks] <- counts[picks] + 1
+      cands <- whonum[ whonum != i & counts < j ]
+      pick <- cands[ sample.int( length(cands), size = 1 ) ]
+      # pick <- sample( cands, 1 ) # This often breaks when length(cands)==1.
+      print(pick)
+      counts[pick] <- counts[pick] + 1
       print(counts)
-      nummat[i, ] <- picks
+      nmat[i, j] <- pick
    }
-   out <- who[ as.vector(nummat) ]
+   # Assign more partners if desired:
+   # No need to check p > 1; the for statement as written will handle that.
+   for ( j in seq_len(p)[-1] ) {
+      for ( i in whonum ) {
+         cands <- whonum[ whonum != i & counts < j & whonum != nmat[i, j - 1] ]
+         cands <- whonum[ whonum != i & counts < j & !is.element(
+            whonum, nmat[i, -j] ) ]
+         pick <- cands[ sample.int( length(cands), size = 1 ) ]
+         print(pick)
+         counts[pick] <- counts[pick] + 1
+         print(counts)
+         nmat[i, j] <- pick
+      }
+   }
+   out <- who[ as.vector(nmat) ]
    dim(out) <- c(n, p)
    out
 }
 
-# Oops!  This not working quite right.  SOMETIMES, it chooses some people
-# > p times and others < p times.  I do not currently see how that is possible!
-
 # Also, add who as rownames to out.
 
-# OK.  Fix this by using sample.int instead of sample.  See Examples under
-# ?sample for an example!!
+# OK.  Now this gave an error :-(
+# hookup(LETTERS[1:4], 2)
+# [1] 4
+# [1] 0 0 0 1
+# [1] 1
+# [1] 1 0 0 1
+# [1] 2
+# [1] 1 1 0 1
+# [1] 3
+# [1] 1 1 1 1
+# [1] 2
+# # [1] 1 2 1 1
+# [1] 4
+# [1] 1 2 1 2
+# [1] 1
+# [1] 2 2 1 2
+# Error in sample.int(length(cands), size = 1) : invalid first argument
 
-# Also, need to modify algorithm to avoid the (somewhat rare) case that I
-# run out of candidates by the last person.  I have a fix for that...
+# MAYBE I should not check counts < j ??? Just check each time that counts < p?
+
